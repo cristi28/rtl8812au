b4f97ee5bd323006b80a0a93a33052e175341c32
atyg02 <aman.tsehaye.girmazion@gmail.com>
2017-03-30 23:44:15 +0200

    add virtual interface

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -3928,6 +3928,62 @@ static const struct net_device_ops rtw_cfg80211_monitor_if_ops = {
 };
 #endif
 
+static int rtw_cfg80211_add_if(_adapter *padapter, char *name, struct net_device **ndev,
+		enum nl80211_iftype type)
+{
+	int ret = 0;
+		
+	if (!name) {
+		DBG_871X(FUNC_ADPT_FMT" without specific name\n", FUNC_ADPT_ARG(padapter));
+		ret = -EINVAL;
+		goto out;
+	}
+	if (!padapter) {
+		DBG_871X(FUNC_ADPT_FMT" without padapter\n", FUNC_ADPT_ARG(padapter));
+		ret = -EINVAL;
+		goto out;
+	}
+	if (padapter->pnetdev) {
+		DBG_871X(FUNC_ADPT_FMT" virtual interface exist\n", FUNC_ADPT_ARG(padapter));
+		ret = -EBUSY;
+		goto out;
+	}
+
+	DBG_871X(FUNC_ADPT_FMT" alloc netdev (padapter=%p, name=%s)\n", FUNC_ADPT_ARG(padapter), padapter, name);
+	if (rtw_os_ndev_init (padapter, name) != _SUCCESS) {
+		DBG_871X(FUNC_ADPT_FMT" error allocating virtual interface\n", FUNC_ADPT_ARG(padapter));
+		ret = -ENODEV;
+		goto out;
+	}
+	if (ndev) *ndev = padapter->pnetdev;
+
+	/* set pointer and type */
+	padapter->pnetdev->ieee80211_ptr = padapter->rtw_wdev;
+	if (padapter->rtw_wdev->iftype != type) {
+		DBG_871X(FUNC_ADPT_FMT" switch iftype to %d\n", FUNC_ADPT_ARG(padapter), type);
+		ret = cfg80211_rtw_change_iface (padapter->rtw_wdev->wiphy, padapter->pnetdev, type, NULL, NULL);
+		if (ret) {
+			DBG_871X(FUNC_ADPT_FMT" errer switching iftype to %d remain with %d\n",
+				FUNC_ADPT_ARG(padapter), type, padapter->rtw_wdev->iftype);
+			goto out;
+		}
+	}
+
+	/* now do register net device */
+	DBG_871X(FUNC_ADPT_FMT" register net device\n", FUNC_ADPT_ARG(padapter));
+	ret = register_netdevice(padapter->pnetdev);
+	if (ret) {
+		DBG_871X(FUNC_ADPT_FMT" register net device\n", FUNC_ADPT_ARG(padapter));
+		free_netdev(padapter->pnetdev);
+		padapter->pnetdev = NULL;
+		goto out;
+	}
+	DBG_871X(FUNC_ADPT_FMT" done\n", FUNC_ADPT_ARG(padapter));
+		
+out:
+	return ret;
+}
+
 static int rtw_cfg80211_add_monitor_if(_adapter *padapter, char *name, struct net_device **ndev)
 {
 	int ret = 0;
@@ -4037,32 +4093,24 @@ static int
 
 	switch (type) {
 	case NL80211_IFTYPE_ADHOC:
+	case NL80211_IFTYPE_AP:
 	case NL80211_IFTYPE_AP_VLAN:
 	case NL80211_IFTYPE_WDS:
 	case NL80211_IFTYPE_MESH_POINT:
-		ret = -ENODEV;
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,37)) || defined(COMPAT_KERNEL_RELEASE)
+	case NL80211_IFTYPE_P2P_CLIENT:
+	case NL80211_IFTYPE_P2P_GO:
+#endif
+	case NL80211_IFTYPE_STATION:
+		ret = rtw_cfg80211_add_if(padapter, (char *)name, &ndev, type);
 		break;
 	case NL80211_IFTYPE_MONITOR:
 		ret = rtw_cfg80211_add_monitor_if(padapter, (char *)name, &ndev);
 		break;
-
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,37)) || defined(COMPAT_KERNEL_RELEASE)
-	case NL80211_IFTYPE_P2P_CLIENT:
-#endif
-	case NL80211_IFTYPE_STATION:
-		ret = -ENODEV;
-		break;
-
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,37)) || defined(COMPAT_KERNEL_RELEASE)
-	case NL80211_IFTYPE_P2P_GO:
-#endif
-	case NL80211_IFTYPE_AP:
-		ret = -ENODEV;
-		break;
 	default:
 		ret = -ENODEV;
-		DBG_871X("Unsupported interface type\n");
-		break;
+		DBG_871X(FUNC_NDEV_FMT" remove monitor interface\n", FUNC_NDEV_ARG(ndev));
 	}
 
 	DBG_871X("%s ndev:%p, ret:%d\n", __func__, ndev, ret);
